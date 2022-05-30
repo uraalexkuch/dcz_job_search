a3b2c1a3a4b95dbe4c2dc38bc8dcd5d206eb4db5
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2022-05-30 09:52:42 +0300

    Add files via upload

---
+++ Header/Header.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+import logo from "../Img/logo_1.png"
+import "./Header.css"
+import {Row} from "react-bootstrap";
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+<>
+               <Row style={{
+                   width:"10%",
+                   marginRight:'1rem',
+                   marginLeft:"1rem"
+               }}>
+                   <img className="logo-img"
+                        src={logo}
+                        height="auto"
+                        width="100"
+                        alt="logo" style={{
+                      marginLeft:"50%",
+                      // marginRight:"2rem"
+                   }}
+
+                /></Row>
+
+                    <div className="hero-text text-center col-auto">
+                        Державний центр зайнятості
+                    </div>
+</>
+
+
+
+)
+    }
+}
