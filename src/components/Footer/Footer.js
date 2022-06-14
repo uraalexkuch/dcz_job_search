7d451932bf166c720c0dd93c9333a66b5dbc9847
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2022-06-14 10:01:38 +0300

    Add files via upload

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,27 @@
 import React, {Component} from 'react';
 import "../Footer/footer.css"
-import logo from "../Img/logo_1.png"
-import { Container, Row} from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  solid, regular,brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
+import { faFacebook,faTelegram,faViber } from "@fortawesome/free-brands-svg-icons"
 
-
+import {faCoffee, faFontAwesome} from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+library.add(  faFontAwesome)
 export default class Footer extends Component {
     render() {
         return (
             <footer className="footer">
                 <div className="container">
-
                         <div className="flex flex-row flex-grow-1 ">
                             <div className="flex-item flex-grow-1 ">
-
-
                                     <div className="table-cell vat" style={{
                                         marginLeft: '2rem',
                                         paddingLeft: '2rem'
                                     }}>
                                         <div className="sitename">Державний центр зайнятості</div>
                                         <div className="address">
-                                            м.Київ, 01601, вул.Еспланадна, 8/10 <p>e-mail:17@es.dcz.gov.ua</p>
+                                         e-mail:17@es.dcz.gov.ua
                                         </div>
 
                                 </div>
@@ -30,11 +30,25 @@ export default class Footer extends Component {
                                 //marginLeft: '2rem',
                                 marginRight: '1rem'
                             }}>
-                                <div className="sitename">Telegram ресурси:</div>
+                                <div className="sitename">Наші ресурси:</div>
                                 <div className="address">
-                                    <a href="https://t.me/worknowdcz" title="">"Робота зараз: Державна служба зайнятості"</a>
-                                    <a href="https://t.me/DSZonline" title="">"Онлайн-підтримка Державної служби зайнятості"</a>
-                                    <a href="https://t.me/+EMOeELVlILE2MWZi" title="">"Державна служба зайнятості"</a>
+                                    <a href="https://t.me/worknowdcz" title=""><FontAwesomeIcon icon={faTelegram} />Робота зараз: Державна служба зайнятості</a>
+                                    <a href="https://t.me/+EMOeELVlILE2MWZi" title=""><FontAwesomeIcon icon={faTelegram} />Державна служба зайнятості</a>
+                                    <a href="https://invite.viber.com/?g2=AQBsEJIHA824hE7mFTxmXPecRQwQDYWtsfdL0DVkBSe8Sycep0JxB8ALDDNPfXfV" title=""><FontAwesomeIcon icon={faViber} />"Державна служба зайнятості"</a>
+
+                                </div>
+
+                            </div>
+                            <div className="table-cell vat" style={{
+                                //marginLeft: '2rem',
+                                marginRight: '2rem'
+                            }}>
+
+                                <div className="sitename">Онлайн-підтримка:</div>
+                                <div className="address">
+                                    <a href="https://t.me/@DCZ_worknow_bot" title=""><FontAwesomeIcon icon={faTelegram}/>Чат-бот </a>
+                                    <a href="https://t.me/DSZonline" title=""><FontAwesomeIcon icon={faTelegram}/>Державна служба зайнятості</a>
+                                    <a href="https://www.facebook.com/zaniatist" title=""><FontAwesomeIcon icon={faFacebook}/>Державна служба зайнятості</a>
                                 </div>
                             </div>
 
