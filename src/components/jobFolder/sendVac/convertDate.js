ff861f0f31d17b51317fd19a232f634db77220b2
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2022-09-16 20:03:19 +0300

    Add files via upload

--- src/components/jobFolder/sendVac/convertDate.js
+++ src/components/jobFolder/sendVac/convertDate.js
@@ -8,22 +8,17 @@ const convertDate = ISOdate => {
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
   // If last post is more than 3 days old, show full date
-  if (diffDays > 3) {
+  if (diffDays > 0) {
     const year = date.getFullYear();
     let month = date.toLocaleString('uk-UA', {
       month: 'numeric',
     });
     month = month.charAt(0).toUpperCase() + month.slice(1);
     let day = date.getDate();
-
-
     // Add zero to days
     if (day < 10) {
       day = '0' + day;
     }
-
-
-
     convertedDate = `${day}. ${month}. ${year}`;
   } else {
     convertedDate = moment(date).fromNow();
