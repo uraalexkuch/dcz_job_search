a6c51764efa564f08aa0b00eece9dc404fb7b6a2
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2022-09-18 21:23:30 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Jobs from './pages/Jobs';
-
 import {theme} from './CustomTheme';
-
 import {ThemeProvider,} from '@mui/material/styles';
-import ScrollToTop from "./components/jobFolder/ScrollToTop";
-import {Router} from "react-router-dom";
+import ReactGA from "react-ga4";
 
+const TRACKING_ID = "G-ZSW5FDZ6JD"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID)
 
 function App() {
     return (
